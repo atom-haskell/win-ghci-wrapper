73a1d629e4ebd5d569f25e5f2f7d875e64f13140
Nikolay Yakimov <root@livid.pp.ru>
2016-10-26 04:59:10 +0300

    Initial commit

---
+++ app/Main.hs
@@ -0,0 +1,37 @@
+module Main where
+
+import System.Process
+import System.Environment
+import System.IO
+import System.Exit
+import Control.Concurrent
+import Control.Monad (filterM)
+
+proc' :: FilePath -> [String] -> CreateProcess
+proc' cmd args = CreateProcess {  cmdspec = RawCommand cmd args,
+                                  cwd = Nothing,
+                                  env = Nothing,
+                                  std_in = CreatePipe,
+                                  std_out = Inherit,
+                                  std_err = Inherit,
+                                  close_fds = False,
+                                  create_group = True,
+                                  delegate_ctlc = True,
+                                  detach_console = False,
+                                  create_new_console = False,
+                                  new_session = False,
+                                  child_group = Nothing,
+                                  child_user = Nothing }
+
+main :: IO ()
+main = do
+  (cmd:args) <- getArgs
+  (Just pin, _, _, pHandle) <- createProcess $ proc' cmd args
+  hSetBuffering pin NoBuffering
+  hSetBuffering stdin NoBuffering
+  forkIO $ do
+    hGetContents stdin >>= mapM_ (f pHandle pin)
+  waitForProcess pHandle >>= exitWith
+  where
+    f h i '\x03' = interruptProcessGroupOf h
+    f _ i c = hPutChar i c
