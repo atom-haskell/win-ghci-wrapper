a5f29e67a8a6f5a0b10e19fb937619270ca21060
Nikolay Yakimov <root@livid.pp.ru>
2020-03-11 16:16:40 +0300

    Tweak and update the code

--- app/Main.hs
+++ app/Main.hs
@@ -7,20 +7,10 @@ import System.Exit
 import Control.Concurrent
 
 proc' :: FilePath -> [String] -> CreateProcess
-proc' cmd args = CreateProcess {  cmdspec = RawCommand cmd args,
-                                  cwd = Nothing,
-                                  env = Nothing,
-                                  std_in = CreatePipe,
-                                  std_out = Inherit,
-                                  std_err = Inherit,
-                                  close_fds = False,
-                                  create_group = True,
-                                  delegate_ctlc = True,
-                                  detach_console = False,
-                                  create_new_console = False,
-                                  new_session = False,
-                                  child_group = Nothing,
-                                  child_user = Nothing }
+proc' cmd args = (proc cmd args) { std_in = CreatePipe,
+                                   create_group = True,
+                                   delegate_ctlc = False
+                                 }
 
 main :: IO ()
 main = do
@@ -28,8 +18,7 @@ main = do
   (Just pin, _, _, pHandle) <- createProcess $ proc' cmd args
   hSetBuffering pin NoBuffering
   hSetBuffering stdin NoBuffering
-  forkIO $ getContents >>= mapM_ (f pHandle pin)
+  let f '\x03' = interruptProcessGroupOf pHandle
+      f c = hPutChar pin c
+  forkIO $ getContents >>= mapM_ f
   waitForProcess pHandle >>= exitWith
-  where
-    f h i '\x03' = interruptProcessGroupOf h
-    f _ i c = hPutChar i c
